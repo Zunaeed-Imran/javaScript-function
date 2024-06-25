a1ca21e4b752cc7a7ce54d5e3618de6f4f0a1ba4
imran <zunaeede@gmail.com>
2024-06-25 15:57:52 +0600

    main - practice input value with name.

--- functio.js
+++ functio.js
@@ -1,3 +1,5 @@
+// practice function call with fname and lname
+
 function greet(name, lastName) {
   document.write('Hello'+name+lastName);
 }
---
+++ function2.js
@@ -0,0 +1,10 @@
+// practice function with input name value
+
+function name1(fname, lname) {
+  document.write("Hello Mr " + fname + " " + lname);
+}
+
+let f = prompt('enter first name');
+let l = prompt('enter last name');
+
+name1(f, l);
