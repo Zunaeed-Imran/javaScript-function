44e1ecea40db9c2ac6f683c8dbec1a486a5ba375
imran <zunaeede@gmail.com>
2024-07-01 13:14:00 +0600

    main - add another practice in PHP file.

---
+++ Max&Min_Number.php
@@ -0,0 +1,17 @@
+<?php
+
+$arrayList = array(10, 200, 3, 40, 500, 60);
+
+$max = $arrayList[0];
+$min = $arrayList[0];
+
+for ($i = 0; $i < count($arrayList); $i++) {
+
+  if ($arrayList[$i] > $max) {
+    $max = $arrayList[$i];
+  } elseif ($arrayList[$i] < $min) {
+    $min = $arrayList[$i];
+  }
+}
+echo $max . "\n";
+echo $min;
